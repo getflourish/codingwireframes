4694723a113a29e8e0690d4676423d1a834b64a9
Florian Schulz <mail@getflourish.com>
2014-03-13 13:37:24 +0100

    Initial

---
+++ js/controllers.js
@@ -0,0 +1,61 @@
+var appControllers = angular.module('appControllers', []);
+ 
+
+appControllers.controller('HomeController', function($http, $scope, $rootScope, $timeout) {
+
+	// load some data
+
+	$scope.data = {};	
+
+	$scope.init = function () {
+		$scope.loadData();
+		$scope.setupKeys();
+		$scope.fakeData = $scope.generateData(100);
+	}
+
+	/** 
+	* Load some data from data.json. Will be available under $scope.data
+	**/
+
+	$scope.loadData = function () {
+		$http.get('data.json').success(function(data) {
+			$scope.data = data;
+		});
+	}
+
+	$scope.setupKeys = function () {
+		key('space', function(){ alert('Hello!') });
+	}
+
+	/**
+	* Generates some fake data using Faker.js
+	* Reference: https://github.com/marak/Faker.js/
+	*
+	* n: Number of data rows that will be generated
+	**/
+
+	$scope.generateData = function(n) {
+ 
+        var fakeData = {"keys":[], "values":[]};
+ 
+        for (var i = 0; i < n; i++) {
+ 
+            var data = {
+                "First Name": Faker.Name.firstName(),
+                "Last Name": Faker.Name.lastName(),
+                "City": Faker.Address.city(),
+                "Color": Faker.random.array_element(["Green", "Red", "Orange"])
+            }
+ 
+            fakeData.values.push(data);
+        }
+        
+        // push keys
+		for(var k in fakeData.values[0]) fakeData.keys.push(k);
+
+        return fakeData;
+    }
+
+	$scope.init();
+
+});
